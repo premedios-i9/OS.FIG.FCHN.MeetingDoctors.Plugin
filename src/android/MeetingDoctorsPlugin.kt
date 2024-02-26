2463002a3455c5f77c9ae4b0165fd13c610f5633
Pedro Remedios <premedios@innovagency.com>
2024-02-26 14:39:26 +0000

    Initial commit

--- src/android/MeetingDoctorsPlugin.kt
+++ src/android/MeetingDoctorsPlugin.kt
@@ -1,3 +1,28 @@
-fun main() {
-    println("Hello, World!")
+package com.fidelidade.meetingdoctors
+
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaArgs
+import org.apache.cordova.CordovaPlugin
+
+class Plugin : CordovaPlugin() {
+    override fun execute(
+            action: String?,
+            args: CordovaArgs?,
+            callbackContext: CallbackContext?
+    ): Boolean {
+        if (action == "echo") {
+            val message: String = args!!.getString(0)
+
+            echo(message, callbackContext)
+        }
+        return super.execute(action, args, callbackContext)
+    }
+
+    private fun echo(message: String, callbackContext: CallbackContext?) {
+        if (message.isNotEmpty()) {
+            callbackContext?.success(message)
+        } else {
+            callbackContext?.error("Expected one non-empty string argument.")
+        }
+    }
 }
